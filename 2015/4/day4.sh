2a1566a6aeffef9ec298297299c893a38a30ef11
addtea <kekitada@gmail.com>
2015-12-06 09:05:45 -0800

    Finish day 4

---
+++ 2015/4/day4.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/zsh
+
+SECRET_KEY="yzbqklnj"
+milestone=10
+i=1
+
+echo "Start time: `date`"
+while true; do
+  if [ $i -eq $milestone ]; then
+    echo "Reached: $i at `date`"
+    milestone=$((milestone*10))
+  fi
+
+  str=""$SECRET_KEY"$i";
+  echo -n $str | md5sum | grep -e "^00000.*"
+  if [ $? -eq 0 ]; then
+    echo "Found solution!"
+    echo "$str at `date`"
+    exit 0
+  fi 
+  i=$((i+1))
+done
